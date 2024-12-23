dfe41ad3a4aeb34039d9b9e56d1ecff1bedc32ac
israr002 <israr002khan@gmail.com>
2024-12-23 19:20:17 +0530

    feat: add support for SVG and configure absolute paths

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,19 @@ module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
     'react-native-reanimated/plugin',
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          components: './src/components',
+          i18n: './src/i18n',
+          assets: './src/assets',
+          screens: './src/screens',
+          navigation: './src/navigation',
+          tests: './tests',
+        },
+      },
+    ],
   ],
 };
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import './gesture-handler';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import initI18next from './app/i18n/initI18next';
+import initI18next from './src/i18n/initI18next';
 
 initI18next();
 AppRegistry.registerComponent(appName, () => App);
