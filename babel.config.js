b88970cdb8407590f3e834eaf58ce264a2f72709
israr002 <israr002khan@gmail.com>
2024-12-23 19:20:17 +0530

    feat: add support for SVG and configure absolute paths

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,15 @@ module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
     'react-native-reanimated/plugin',
+    ['module-resolver', {
+      alias: {
+        components: './src/components',
+        i18n: './src/i18n',
+        assets: './src/assets',
+        screens: './src/screens',
+        navigation: './src/navigation',
+        tests: './tests',
+      },
+    }],
   ],
 };
